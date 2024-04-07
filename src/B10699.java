0c61b6fef1b967ba0d4a532ac2bb88a0e253feb9
gongonii <pje6217@naver.com>
2024-04-07 13:28:22 +0900

    B10699 현재날짜

---
+++ src/B10699.java
@@ -0,0 +1,10 @@
+import java.time.LocalDate; // 현재 날짜 현재시간
+
+public class B10699 {
+    public static void main(String[] args) {
+        LocalDate now = LocalDate.now();
+
+        System.out.println(now);
+    }
+
+}
