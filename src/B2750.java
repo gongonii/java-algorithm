aa023e403a6ca3cfd65c0a7d01a36effa5bb8d8e
gongonii <pje6217@naver.com>
2025-03-12 18:30:52 +0900

    B2750 - 수 정렬하기

---
+++ src/B2750.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class B2750 {
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+
+        int count = sc.nextInt();
+        int num[] = new int[count];
+
+        for(int i = 0; i<count; i++)
+        {
+            num[i] = sc.nextInt();
+        }
+
+        for(int i = 0; i<num.length; i++) //선택정렬 알고리즘
+        {
+            for(int j = i; j<num.length; j++)
+            {
+                if(num[j] < num[i])
+                {
+                    int temp = num[j];
+                    num[j] = num[i];
+                    num[i] = temp;
+                }
+            }
+        }
+        for(int i = 0; i<num.length; i++)
+            System.out.println(num[i]);
+    }
+}
